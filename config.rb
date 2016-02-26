d623426c21e768b48078f68123d3e9c05bd077a9
jeremyjousse <jeremy.jousse@coevo.fr>
2016-02-26 13:38:18 +0100

    Fix locals when building

--- config.rb
+++ config.rb
@@ -1,24 +1,7 @@
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
-
-# General configuration
-
-# Reload the browser automatically whenever files change
 require 'lib/custom_helpers'
 helpers CustomHelpers
 
@@ -29,12 +12,14 @@ end
 activate :i18n,
          path: '/:locale/',
          lang_map: { en: :en, fr: :fr },
-         mount_at_root: :en
+         mount_at_root: :en,
+         langs: [:en, :fr]
 
 activate :autoprefixer
 
+set :haml, ugly: true, format: :html5
+
 activate :blog do |blog|
-  # blog.prefix = 'posts'
   blog.sources = 'posts/{lang}/{year}-{month}-{day}-{title}.html'
   blog.permalink = 'blog/{year}-{month}-{day}-{title}.html'
   blog.default_extension = '.md'
@@ -50,24 +35,11 @@ end
 
 require 'font-awesome-sass'
 
-# activate :middleman_scavenger do |config|
-#   config.path = './source/svg/'
-#   config.prefix = 'icon-'
-#   config.sprite_path = 'svg-sprite/sprite.svg'
-# end
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
 configure :build do
+  ignore 'readme.md'
   activate :asset_hash
   activate :minify_javascript
   activate :minify_css
-  activate :gzip
 end
 
 activate :deploy do |deploy|
